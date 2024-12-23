319a24fd3948412b5fa41958378005e3abb6556a
FernandoZuchi <nandozuchi07@gmail.com>
2024-12-22 21:32:03 -0300

    ✨ feat: validations

--- CashFlow/src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ CashFlow/src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,4 +1,5 @@
-﻿using CashFlow.Communication.Requests;
+﻿using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
 
 namespace CashFlow.Application.UseCases.Expenses.Register;
@@ -7,7 +8,34 @@ public class RegisterExpenseUseCase
 {
     public ResponseRegisteredExpenseJson Execute(RequestRegisterExpenseJson request) 
     {
-        // TO-DO VALIDATIONS 
+        Validate(request);
+
         return new ResponseRegisteredExpenseJson();
+    }
+
+    private void Validate (RequestRegisterExpenseJson request)
+    {
+        var titleIsEmpty = string.IsNullOrEmpty(request.Title);
+        if (titleIsEmpty)
+        {
+            throw new ArgumentException("The title is required.");
+        }
+
+        if (request.Amount <= 0)
+        {
+            throw new ArgumentException("The amount must be greater then zero.");
+        }
+
+        var result = DateTime.Compare(request.Date, DateTime.UtcNow);
+        if (result > 0) 
+        {
+            throw new ArgumentException("Expenses cannot be for the future");
+        }
+
+        var paymentTypeIsValid = Enum.IsDefined(typeof(PaymentType), request.PaymentType);
+        if (paymentTypeIsValid == false)
+        {
+            throw new ArgumentException("You must choose a valid payment type.");
+        }
     }
 }
--- CashFlow/src/CashFlow.Application/obj/Debug/net8.0/CashFlow.Application.AssemblyInfo.cs
+++ CashFlow/src/CashFlow.Application/obj/Debug/net8.0/CashFlow.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CashFlow.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1511d8ee2a46e7ee2ab72e1109336938f984b7e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d10210e608d03e9219124759f009e04a7e880b45")]
 [assembly: System.Reflection.AssemblyProductAttribute("CashFlow.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CashFlow.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
