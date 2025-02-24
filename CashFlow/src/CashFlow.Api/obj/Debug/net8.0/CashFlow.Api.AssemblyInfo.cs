73ffa88d89535affaddc66edead3c97e25fab9b4
FernandoZuchi <nandozuchi07@gmail.com>
2025-02-24 19:55:47 -0300

    ♻️ refactor: modularizando filtro de exceções

--- CashFlow/src/CashFlow.Api/obj/Debug/net8.0/CashFlow.Api.AssemblyInfo.cs
+++ CashFlow/src/CashFlow.Api/obj/Debug/net8.0/CashFlow.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CashFlow.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9895be1b516b54b14e182edd8be72596521dba3d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7ac20d2379e0b8cc7a8c916bdf79ffce9dbed34")]
 [assembly: System.Reflection.AssemblyProductAttribute("CashFlow.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CashFlow.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
